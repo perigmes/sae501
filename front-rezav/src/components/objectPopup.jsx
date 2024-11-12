ba81a2b98b571708bbdebf57218c5b71a81c9343
Pierrickbrd <Pierrick.Breaud.Etu@univ-lemans.fr>
2024-11-12 12:14:40 +0100

    bug conflit fichier

--- front-rezav/src/components/objectPopup.jsx
+++ front-rezav/src/components/objectPopup.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useDispatch, useSelector } from "react-redux";
 import { selectObjInfos } from "../features/demande/demandeSelector";
 import { setInfoObject } from "../features/demande/demandeSlice";
@@ -17,23 +16,3 @@ const ObjectPopup = () => {
 };
 
 export default ObjectPopup;
-=======
-import { useDispatch, useSelector } from "react-redux";
-import { selectObjInfos } from "../features/demande/demandeSelector";
-import { setInfoObject } from "../features/demande/demandeSlice";
-
-const ObjectPopup = () => {
-    const dispatch = useDispatch();
-
-    const closePopup = () => {
-            dispatch(setInfoObject({}));
-    };
-
-    return(
-        <div className="object-popup" onClick={closePopup}>
-        </div>
-    );
-};
-
-export default ObjectPopup;
->>>>>>> f0429426010dae39588239ea01f6707666b3e27f
