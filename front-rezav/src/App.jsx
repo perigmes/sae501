c20ad686a99a03d255330008c31e27fb49181f83
QW6D3 <charlie.charron.etu@univ-lemans.fr>
2024-11-15 16:24:51 +0100

    resolved conflict app.jsx

--- front-rezav/src/App.jsx
+++ front-rezav/src/App.jsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import "material-icons/iconfont/round.css" 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Ceci est un test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-export default App;
-=======
 // src/App.jsx
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
