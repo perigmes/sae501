65d1cff7780780c955a667fe30a5ecaa63f16258
QW6D3 <charlie.charron.etu@univ-lemans.fr>
2024-11-15 16:23:20 +0100

    Pull maj

--- front-rezav/src/features/store.js
+++ front-rezav/src/features/store.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { configureStore } from "@reduxjs/toolkit";
 import filmReducer from './film/filmSlice'
 
@@ -6,15 +5,4 @@ const store = configureStore({
     reducer: {
     }
 })
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import demandeReducer from './demande/demandeSlice'
-
-const store = configureStore({
-    reducer: {
-        demande: demandeReducer,
-    }
-})
-
->>>>>>> 38c464372ebf750c606c07e33d1c6184989f7512
 export default store;
