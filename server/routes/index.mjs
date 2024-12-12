b696c7afa6b1888bd824cbaf35eb53fc7d194617
perigmes <clemchallans@gmail.com>
2024-12-12 15:18:40 +0100

    maj route and matos

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,14 +1,16 @@
 import express from "express";
 
-import { PostReservation, GetReservation, UpdateReservationStatus } from "../controllers/reservationController.mjs";
-import { GetItems,DeleteStateItem,GetItemById } from "../controllers/itemController.mjs";
+import { PostReservation, GetReservation, UpdateReservationStatus,GetReservationsByUserId } from "../controllers/reservationController.mjs";
+import { GetItems,DeleteStateItem,GetItemById,EditItem } from "../controllers/itemController.mjs";
 
 export const router = express.Router();
 
 router.get("/items", GetItems);
 router.get("/items/:id", GetItemById);
 router.patch("/items/state", DeleteStateItem);
+router.patch("/items/:id", EditItem);
 router.post("/reservation", PostReservation);
 router.get("/reservation", GetReservation);
+router.get("/reservation/user/:userId", GetReservationsByUserId);
 router.patch("/reservation/requestStatus/:id", UpdateReservationStatus);
 
